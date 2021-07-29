00095cab28b80edf75e7cc21ea9cdd2904eff825
José Fernandes de Freitas Junior <jffreitasjr@yahoo.com.br>
2021-07-28 22:51:48 -0300

    Adicionando search de categoria e de hortaliça

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import Logo from '../../assets/Logo.svg'
+import SearchHortalica from '../../assets/SearchHortalica.svg'
+import SearchCategory from '../../assets/SearchCategory.svg'
+import { TriangleDownIcon } from '@chakra-ui/icons'
 import { Flex,
          Image,
-         Box
+         Box,
+         IconButton
        } 
        from "@chakra-ui/react"
 
@@ -14,19 +18,75 @@ export const Home = () => {
             justifyContent='space-between'
             minHeight='100vh'
             
+            
         >
             <Flex 
                 bg='#FFF'
+                paddingY='10px' 
+             
+                
             >
 
                 <Image src={ Logo } 
+                       alt='Hortinja Logo' 
+                       marginX='4'
+                       marginX='170px'
+                       
+                       
+                />
+                {/* <Image src={ SearchCategory } 
                        alt='Fanpics Logo' 
                        marginX='4'
-                       marginX='200px'
-                       paddingY='10px' 
+                      
                 />
+                <Image src={ SearchHortalica } 
+                alt='Fanpics Logo' 
+                marginX='4'
+                
+                       
+                       
+                /> */}
+                
+                <Box bg="#E5E5E5" 
+                     w="20%" 
+                     p={5} 
+                     color='#757270'
+                     borderRadius='40px'
+                     marginRight='15px'
+                     justifyContent='space-between'
+                    
+                     
+                    
+                >
+                   Selecionar categoria
+                   <IconButton 
+                        aria-label='Selecionar Categoria' 
+                        icon={<TriangleDownIcon/>}    
+                        variant='ghost' 
+                        marginLeft= '95px'  
+                        color='black'               
+                    />
+                </Box>
 
-                Espaço para os 2 campos de busca
+                <Box bg="#E5E5E5" 
+                     w="20%" 
+                     p={5} 
+                     color='#757270'
+                     borderRadius='40px'
+                     marginRight='15px'
+                     justifyContent='space-between'
+                >
+                   Buscar hortaliça
+                   <IconButton 
+                        aria-label='Selecionar Categoria' 
+                        icon={<TriangleDownIcon/>}    
+                        variant='ghost' 
+                        marginLeft= '120px'  
+                        color='black'               
+                    />
+                </Box>
+
+                
 
             </Flex>
 
